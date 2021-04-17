10bd5a3a22d41d39d4de6948d4cf1854f7af3e30
Chen <rc185204@ncr.com>
2021-04-17 10:30:05 +0800

    添加项目文件。

---
+++ DbContext/Source/FamilyRelationshipContext.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FRS.DbContext
+{
+    public class FamilyRelationshipContext
+    {
+    }
+}
---
+++ Models/Source/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FRS.Models
+{
+    public class Class1
+    {
+    }
+}
