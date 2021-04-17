8bd0b54f2cf64a15b1416876f2cccf1cd43a4cac
Archer-ChenRonghua <839634083@qq.com>
2021-04-17 14:31:18 +0800

    Update User.cs

--- Models/Source/User.cs
+++ Models/Source/User.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace FRS.Models
 {
-    public class User
+    class User
     {
-        public User() { }
     }
 }
